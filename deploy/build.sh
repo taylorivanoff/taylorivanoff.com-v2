2e4454082db3be65df88a36e5311c122193423c5
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-05-31 19:46:04 +1000

    Deploy changes.

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,5 +4,6 @@
 docker builder prune -f
 docker container prune -f
 
-docker compose -f '../docker-compose.yml' rm
-docker compose -f '../docker-compose.yml' up -d
+docker compose down
+docker compose build
+docker compose up -d
