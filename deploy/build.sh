f561e7c5cd0761ad8f37070f612e1860978525b4
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-05-31 18:03:42 +1000

    Deploy changes.

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "The current branch is ${CI_COMMIT_BRANCH}"
-echo "The current commit hash is ${CI_COMMIT_SHA}"
-echo "Building..."
 docker builder prune -f # don't add -a here, it will destroy build cache
 docker container prune -f
 docker compose -f docker-compose.yml up -d --build --remove-orphans
-echo "Building done."
