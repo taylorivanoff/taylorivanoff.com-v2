9097aef86ff79b77f9e25adc8c262019894138e4
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-05-31 17:13:11 +1000

    Deploy changes.

--- deploy/build.sh
+++ deploy/build.sh
@@ -4,5 +4,4 @@ echo "The current branch is ${CI_COMMIT_BRANCH}"
 echo "The current commit hash is ${CI_COMMIT_SHA}"
 echo "Building done."
 
-#
 #docker compose -f docker-compose.yml up -d --build --remove-orphans
