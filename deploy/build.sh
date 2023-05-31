428931a2cb7fb700f51bb36fd277de955ae26fe1
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-05-31 20:49:12 +1000

    Deploy changes.

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # Clear caches
-#docker builder prune -f
-#docker container prune -f
+docker builder prune -f
+docker container prune -f
 
 pwd
 ls -la
+
 docker compose down
 docker compose rm -f
+
 docker compose up -d --build --force-recreate
+
+docker compose exec app composer install
+docker compose exec app cp .env.example .env
