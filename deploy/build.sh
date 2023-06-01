09534acb567e2973ba4a0e53c6fc601da791c085
Taylor Ivanoff <taylor.ivanoff@vocus.com.au>
2023-06-01 11:24:32 +1000

    Deploy changes.

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-pwd
-cd /woodpecker/src/github.com/taylorivanoff/taylorivanoff.com || return
-
-# Clear caches
-docker builder prune -f
-docker container prune -f
-
-docker compose down
-docker compose rm -f
-
-docker compose up -d --build --force-recreate
-
-pwd
-cd deploy || return
-composer install
-#      - cp .env.example .env
+docker compose build
+docker -f docker-compose.yml -f deploy/production.yml compose up --no-deps -d
